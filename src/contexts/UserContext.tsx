9488175ab206ddff854c82883ea800e6f7d7bec2
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:25:38 +0530

    First Commit

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -3,73 +3,84 @@ import React, { createContext, useState, useEffect } from 'react';
 import { UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
 import { getUserProfile, updateUserProfile } from '../apis/ProfileApi';
 
+// Step 1: Create the UserContextProps interface
 interface UserContextProps {
-  userProfile: UserProfileResponse | null;
+  user: UserProfileResponse | null;
   loading: boolean;
   error: string | null;
   getUserProfile: () => void;
   updateUserProfile: (request: UserProfileUpdateRequest) => Promise<UserProfileUpdateResponse>;
 }
 
-export const UserContext = createContext<UserContextProps>({
-  userProfile: null,
+// Step 2: Create the initial context values
+const initialContext: UserContextProps = {
+  user: null,
   loading: false,
   error: null,
   getUserProfile: () => {},
   updateUserProfile: () => Promise.resolve({ success: false, message: '' }),
-});
+};
 
+// Step 3: Create the UserContext
+export const UserContext = createContext<UserContextProps>(initialContext);
+
+// Step 4: Create the UserProvider component
 export const UserProvider: React.FC = ({ children }) => {
-  const [userProfile, setUserProfile] = useState<UserProfileResponse | null>(null);
+  const [user, setUser] = useState<UserProfileResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    getUserProfile();
-  }, []);
-
+  // Step 5: Implement the getUserProfile function
   const getUserProfile = () => {
     setLoading(true);
     setError(null);
 
     getUserProfile()
-      .then((response) => {
+      .then((response: UserProfileResponse) => {
+        setUser(response);
         setLoading(false);
-        setUserProfile(response.user);
       })
-      .catch((error) => {
+      .catch((error: string) => {
+        setError(error);
         setLoading(false);
-        setError(error.message);
       });
   };
 
+  // Step 6: Implement the updateUserProfile function
   const updateUserProfile = (request: UserProfileUpdateRequest) => {
     setLoading(true);
     setError(null);
 
     return updateUserProfile(request)
-      .then((response) => {
+      .then((response: UserProfileUpdateResponse) => {
+        if (response.success) {
+          // Update the user object with the updated profile
+          setUser((prevUser) => ({
+            ...prevUser,
+            name: request.name || prevUser.name,
+            contactInfo: request.contactInfo || prevUser.contactInfo,
+            address: request.address || prevUser.address,
+            profilePicture: request.profilePicture || prevUser.profilePicture,
+          }));
+        }
         setLoading(false);
-        setUserProfile(response.user);
         return response;
       })
-      .catch((error) => {
+      .catch((error: string) => {
+        setError(error);
         setLoading(false);
-        setError(error.message);
-        return { success: false, message: error.message };
+        return { success: false, message: error };
       });
   };
 
+  // Step 7: Implement the useEffect hook to fetch user profile on component mount
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
+  // Step 8: Return the UserContext.Provider with the context values
   return (
-    <UserContext.Provider
-      value={{
-        userProfile,
-        loading,
-        error,
-        getUserProfile,
-        updateUserProfile,
-      }}
-    >
+    <UserContext.Provider value={{ user, loading, error, getUserProfile, updateUserProfile }}>
       {children}
     </UserContext.Provider>
   );
