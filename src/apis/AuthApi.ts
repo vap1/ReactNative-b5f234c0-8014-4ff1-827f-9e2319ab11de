1f07b40d07012559848b783b6b2020f27f53c912
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:14:05 +0530

    First Commit

--- src/apis/AuthApi.ts
+++ src/apis/AuthApi.ts
@@ -3,34 +3,21 @@ import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 
 const loginUser = async (request: UserLoginRequest): Promise<UserLoginResponse> => {
   try {
-    // Log the request details
-    console.log('Login API Request:', request);
+    console.log('Calling loginUser API');
+    console.log('Request:', request);
 
-    // Generate random data for the response
+    // Simulating API call and generating random data
     const response: UserLoginResponse = {
       success: true,
       message: 'Login successful',
-      token: 'random_token',
+      token: 'randomToken123',
     };
 
-    // Log the response details
-    console.log('Login API Response:', response);
-
+    console.log('Response:', response);
     return response;
   } catch (error) {
-    // Log any errors
-    console.error('Login API Error:', error);
-
-    // Generate random error message for the response
-    const response: UserLoginResponse = {
-      success: false,
-      message: 'An error occurred during login',
-    };
-
-    // Log the response details
-    console.log('Login API Response:', response);
-
-    return response;
+    console.error('Error in loginUser API:', error);
+    throw error;
   }
 };
 
