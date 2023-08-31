583c1ed27fdc766ea03661aa04443f66f0efa53e
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:54:30 +0530

    First Commit

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,6 +1,8 @@
 
 // File: src/types/Types.ts
 
+console.log('Types.ts: Start');
+
 export interface User {
   name: string;
   email: string;
@@ -9,21 +11,29 @@ export interface User {
   profilePicture?: string;
 }
 
+console.log('Types.ts: User interface defined');
+
 export interface UserRegistrationRequest {
   name: string;
   email: string;
   password: string;
 }
 
+console.log('Types.ts: UserRegistrationRequest interface defined');
+
 export interface UserRegistrationResponse {
   success: boolean;
   message: string;
 }
 
+console.log('Types.ts: UserRegistrationResponse interface defined');
+
 export interface UserLoginRequest {
   email: string;
   password: string;
 }
+
+console.log('Types.ts: UserLoginRequest interface defined');
 
 export interface UserLoginResponse {
   success: boolean;
@@ -31,13 +41,19 @@ export interface UserLoginResponse {
   token?: string;
 }
 
+console.log('Types.ts: UserLoginResponse interface defined');
+
 export interface UserProfileRequest {
   token: string;
 }
 
+console.log('Types.ts: UserProfileRequest interface defined');
+
 export interface UserProfileResponse {
   user: User;
 }
+
+console.log('Types.ts: UserProfileResponse interface defined');
 
 export interface UserProfileUpdateRequest {
   token: string;
@@ -47,15 +63,25 @@ export interface UserProfileUpdateRequest {
   profilePicture?: string;
 }
 
+console.log('Types.ts: UserProfileUpdateRequest interface defined');
+
 export interface UserProfileUpdateResponse {
   success: boolean;
   message: string;
 }
 
+console.log('Types.ts: UserProfileUpdateResponse interface defined');
+
 export interface AdminUserDetailsRequest {
   token: string;
 }
 
+console.log('Types.ts: AdminUserDetailsRequest interface defined');
+
 export interface AdminUserDetailsResponse {
   users: User[];
 }
+
+console.log('Types.ts: AdminUserDetailsResponse interface defined');
+
+console.log('Types.ts: End');
