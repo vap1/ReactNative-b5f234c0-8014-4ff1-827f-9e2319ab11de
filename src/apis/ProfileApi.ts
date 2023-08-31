302a0f446767645d45464c559abb53ece1043652
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:14:09 +0530

    First Commit

--- src/apis/ProfileApi.ts
+++ src/apis/ProfileApi.ts
@@ -7,31 +7,26 @@ const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileR
     console.log('Calling getUserProfile API');
 
     // Generate random user profile data
-    const userProfile = {
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      contactInfo: '1234567890',
-      address: '123 Main St',
-      profilePicture: 'https://example.com/profile.jpg',
+    const userProfile: UserProfileResponse = {
+      user: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        contactInfo: '1234567890',
+        address: '123 Main St',
+        profilePicture: 'https://example.com/profile.jpg',
+      },
     };
 
-    // Simulate API call delay
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // Log the response
+    console.log('getUserProfile API response:', userProfile);
 
-    // Return the random user profile data
-    return {
-      success: true,
-      user: userProfile,
-    };
+    return userProfile;
   } catch (error) {
     // Log the error
     console.error('Error in getUserProfile API:', error);
 
-    // Return an error response
-    return {
-      success: false,
-      message: 'Failed to get user profile',
-    };
+    // Throw the error
+    throw error;
   }
 };
 
