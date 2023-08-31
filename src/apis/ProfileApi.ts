952674ccd448d3818f5ea2b3af8ab0d764cb3678
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:25:36 +0530

    First Commit

--- src/apis/ProfileApi.ts
+++ src/apis/ProfileApi.ts
@@ -4,36 +4,20 @@ import { UserProfileRequest, UserProfileResponse } from '../types/Types';
 const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
   try {
     console.log('Fetching user profile...');
-    // Make API call to fetch user profile
-    // Replace the following code with your actual API call
-    const response = await fetch('/api/profile', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${request.token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch user profile');
-    }
-
-    // Generate random user profile data for testing
-    const userProfile: UserProfileResponse = {
-      user: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        contactInfo: '1234567890',
-        address: '123 Main St',
-        profilePicture: 'https://example.com/profile.jpg',
-      },
+    // Simulate API call and generate random data
+    const userProfile = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      contactInfo: '1234567890',
+      address: '123 Main St',
+      profilePicture: 'https://example.com/profile.jpg',
     };
 
     console.log('User profile fetched successfully:', userProfile);
-    return userProfile;
+    return { user: userProfile };
   } catch (error) {
-    console.error('Error fetching user profile:', error);
-    throw error;
+    console.error('Failed to fetch user profile:', error);
+    throw new Error('Failed to fetch user profile');
   }
 };
 
