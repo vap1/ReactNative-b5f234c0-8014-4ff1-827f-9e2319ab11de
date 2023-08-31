7a26cadd43c9ab031f6cf5f1d31cf3df59900be8
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:21:42 +0530

    First Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,63 +1,54 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import { View, TextInput, Button, Text } from 'react-native';
 import { loginUser, UserLoginRequest, UserLoginResponse } from '../apis/AuthApi';
 import { useAuthContext } from '../contexts/AuthContext';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { setToken } = useAuthContext();
+  const { setLoggedIn } = useAuthContext();
 
   const handleLogin = async () => {
+    console.log('Login form submitted');
     try {
-      // Step 1: Validate input
-      if (!email || !password) {
-        Alert.alert('Error', 'Please enter email and password');
-        return;
-      }
-
-      // Step 2: Create login request
-      const loginRequest: UserLoginRequest = {
+      const request: UserLoginRequest = {
         email,
         password,
       };
 
-      // Step 3: Make API call to login user
-      const response: UserLoginResponse = await loginUser(loginRequest);
+      console.log('Sending login request:', request);
+      const response: UserLoginResponse = await loginUser(request);
+      console.log('Received login response:', response);
 
-      // Step 4: Handle API response
       if (response.success) {
-        // Step 5: Set token in context
-        setToken(response.token);
-        Alert.alert('Success', 'Login successful');
+        console.log('Login successful');
+        setLoggedIn(true);
       } else {
-        Alert.alert('Error', response.message);
+        console.log('Login failed:', response.message);
       }
     } catch (error) {
-      console.error('Error occurred during login:', error);
-      Alert.alert('Error', 'An error occurred during login');
+      console.log('Error occurred during login:', error);
     }
   };
 
   return (
     <View>
-      {/* Step 6: Render email input */}
+      <Text>Email:</Text>
       <TextInput
-        placeholder="Email"
         value={email}
         onChangeText={setEmail}
+        placeholder="Enter your email"
       />
 
-      {/* Step 7: Render password input */}
+      <Text>Password:</Text>
       <TextInput
-        placeholder="Password"
         value={password}
         onChangeText={setPassword}
+        placeholder="Enter your password"
         secureTextEntry
       />
 
-      {/* Step 8: Render login button */}
       <Button title="Login" onPress={handleLogin} />
     </View>
   );
