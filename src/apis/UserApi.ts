2e62c0cfe58493f080e8b839cce378953e9b393e
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:33:29 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -1,51 +1,24 @@
 
-import { UserRegistrationRequest, UserRegistrationResponse, UserLoginRequest, UserLoginResponse } from '../types/Types';
+import { UserRegistrationRequest, UserRegistrationResponse } from '../types/Types';
 
 const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
   try {
-    // Log the registration request
-    console.log('Registering user:', request);
-
-    // Generate a random success status
-    const success = Math.random() < 0.5;
-
-    // Generate a random message
-    const message = success ? 'User registration successful' : 'User registration failed';
-
-    // Return the response
-    return { success, message };
+    console.log('Calling registerUser API');
+    // Generate random data for success response
+    const response: UserRegistrationResponse = {
+      success: true,
+      message: 'User registration successful',
+    };
+    return response;
   } catch (error) {
-    // Log the error
-    console.error('Error registering user:', error);
-
-    // Return an error response
-    return { success: false, message: 'An error occurred during user registration' };
+    console.error('Error in registerUser API:', error);
+    // Generate random data for error response
+    const response: UserRegistrationResponse = {
+      success: false,
+      message: 'User registration failed',
+    };
+    return response;
   }
 };
 
-const loginUser = async (request: UserLoginRequest): Promise<UserLoginResponse> => {
-  try {
-    // Log the login request
-    console.log('Logging in user:', request);
-
-    // Generate a random success status
-    const success = Math.random() < 0.5;
-
-    // Generate a random message
-    const message = success ? 'User login successful' : 'User login failed';
-
-    // Generate a random token
-    const token = success ? 'random-token' : '';
-
-    // Return the response
-    return { success, message, token };
-  } catch (error) {
-    // Log the error
-    console.error('Error logging in user:', error);
-
-    // Return an error response
-    return { success: false, message: 'An error occurred during user login', token: '' };
-  }
-};
-
-export { registerUser, loginUser };
+export default registerUser;
