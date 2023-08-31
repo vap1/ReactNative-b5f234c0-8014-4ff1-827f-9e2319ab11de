3c88bc84b617d950ef30ef75d5a042c5c4f89cb6
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:58:49 +0530

    First Commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,50 +1,43 @@
 
 import React, { createContext, useState } from 'react';
-import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 import { loginUser } from '../apis/AuthApi';
+import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 
+// Step 1: Create the AuthContext
 interface AuthContextProps {
-  isLoggedIn: boolean;
+  user: UserLoginResponse | null;
   login: (email: string, password: string) => Promise<void>;
-  logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextProps>({
-  isLoggedIn: false,
+  user: null,
   login: async () => {},
-  logout: () => {},
 });
 
-const AuthContextProvider: React.FC = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+// Step 2: Create the AuthProvider component
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<UserLoginResponse | null>(null);
 
+  // Step 3: Implement the login function
   const login = async (email: string, password: string) => {
     try {
-      // Make API call to login user
-      const loginRequest: UserLoginRequest = { email, password };
-      const response: UserLoginResponse = await loginUser(loginRequest);
+      // Step 4: Make the API call to login
+      const request: UserLoginRequest = { email, password };
+      const response = await loginUser(request);
 
-      if (response.success) {
-        setIsLoggedIn(true);
-        console.log('User logged in successfully');
-      } else {
-        console.log('Login failed:', response.message);
-      }
+      // Step 5: Update the user state
+      setUser(response);
+
+      console.log('Login successful');
     } catch (error) {
-      console.log('Error occurred during login:', error);
+      console.error('Login failed:', error);
     }
   };
 
-  const logout = () => {
-    setIsLoggedIn(false);
-    console.log('User logged out');
-  };
-
+  // Step 6: Provide the context value to the children components
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ user, login }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
-export default AuthContextProvider;
