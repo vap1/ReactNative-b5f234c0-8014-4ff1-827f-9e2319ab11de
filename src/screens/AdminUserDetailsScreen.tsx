33c8050ec945d3aa3726b74edb519683d3b9fc11
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:17:53 +0530

    First Commit

--- src/screens/AdminUserDetailsScreen.tsx
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -2,21 +2,29 @@
 import React, { useEffect, useContext } from 'react';
 import { View, Text } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
+import { getUsers, User } from '../apis/AdminApi';
 
 const AdminUserDetailsScreen: React.FC = () => {
   const { users, getUsers } = useContext(UserContext);
 
   useEffect(() => {
-    console.log('Fetching user details...');
-    getUsers();
-  }, []);
+    const fetchUsers = async () => {
+      try {
+        console.log('Fetching users...');
+        const response = await getUsers();
+        console.log('Users:', response.users);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
 
-  console.log('Rendering AdminUserDetailsScreen...');
+    fetchUsers();
+  }, []);
 
   return (
     <View>
       <Text>Admin User Details</Text>
-      {users.map((user) => (
+      {users.map((user: User) => (
         <View key={user.email}>
           <Text>Name: {user.name}</Text>
           <Text>Email: {user.email}</Text>
