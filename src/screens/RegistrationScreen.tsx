21dc49bc31567392684295f3b8c8ca2ed07a3cc9
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 02:39:40 +0530

    First Commit

---
+++ src/screens/RegistrationScreen.tsx
@@ -0,0 +1,66 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { registerUser } from '../apis/UserApi';
+
+const RegistrationScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegistration = async () => {
+    try {
+      console.log('Submitting registration form...');
+      const response = await registerUser({ name, email, password });
+      console.log('Registration response:', response);
+      // Handle success or display error message to the user
+    } catch (error) {
+      console.error('Registration error:', error);
+      // Display error message to the user
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Register" onPress={handleRegistration} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
+
+export default RegistrationScreen;
