1a012769f80d10c7e8411c872af00617e5c4f11a
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:21:47 +0530

    First Commit

--- src/apis/ProfileUpdateApi.ts
+++ src/apis/ProfileUpdateApi.ts
@@ -3,7 +3,6 @@ import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Ty
 
 const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
   try {
-    // Log the request details
     console.log('Updating user profile...');
     console.log('Request:', request);
 
@@ -13,19 +12,11 @@ const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<Use
       message: 'User profile updated successfully.',
     };
 
-    // Log the response details
     console.log('Response:', response);
-
     return response;
   } catch (error) {
-    // Log any errors
     console.error('Error updating user profile:', error);
-
-    // Return an error response
-    return {
-      success: false,
-      message: 'Failed to update user profile.',
-    };
+    throw error;
   }
 };
 
