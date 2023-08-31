63f2c926e003ec81c198bd7e86d16873628e3609
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:54:27 +0530

    First Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,45 +1,30 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
-import { UserLoginRequest, UserLoginResponse } from '../types/Types';
-import { loginUser } from '../apis/AuthApi';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-const LoginForm: React.FC = () => {
+interface LoginFormProps {
+  onLogin: (email: string, password: string) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      // Log the login request
-      console.log('Login request:', { email, password });
-
-      // Make the API call to login
-      const request: UserLoginRequest = { email, password };
-      const response: UserLoginResponse = await loginUser(request);
-
-      // Log the login response
-      console.log('Login response:', response);
-
-      // Update the user token if login is successful
-      if (response.success) {
-        console.log('User login successful');
-      } else {
-        console.log('User login failed');
-      }
-    } catch (error) {
-      // Log any errors that occur during login
-      console.error('Login error:', error);
-    }
+  const handleLogin = () => {
+    console.log('Logging in...');
+    onLogin(email, password);
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
@@ -49,5 +34,21 @@ const LoginForm: React.FC = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
 
 export default LoginForm;
