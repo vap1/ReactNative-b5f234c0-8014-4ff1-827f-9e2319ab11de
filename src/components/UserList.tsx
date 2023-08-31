343d11c33ac5083f67fb0f7562046a6d8fa3f5da
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:54:26 +0530

    First Commit

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,48 +1,70 @@
 
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { User } from '../types/Types';
 import { getAdminUserDetails } from '../apis/AdminApi';
+import { AuthContext } from '../contexts/AuthContext';
+import { UserContext } from '../contexts/UserContext';
 
 const UserList: React.FC = () => {
+  const { token } = useContext(AuthContext);
+  const { isAdmin } = useContext(UserContext);
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUserDetails = async () => {
+    const fetchUsers = async () => {
       try {
-        // Log the admin user details request
-        console.log('Admin user details request');
-
-        // Make the API call to get admin user details
-        const request: AdminUserDetailsRequest = { token: 'adminToken123' };
-        const response: AdminUserDetailsResponse = await getAdminUserDetails(request);
-
-        // Log the admin user details response
-        console.log('Admin user details response:', response);
-
-        // Update the users state with the fetched user details
-        setUsers(response.users);
+        setLoading(true);
+        const response = await getAdminUserDetails(token);
+        if (response.success) {
+          setUsers(response.users);
+        } else {
+          console.log('Failed to fetch user details:', response.message);
+        }
       } catch (error) {
-        // Log any errors that occur during fetching admin user details
-        console.error('Admin user details error:', error);
+        console.log('Error fetching user details:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchUserDetails();
-  }, []);
+    if (isAdmin) {
+      fetchUsers();
+    }
+  }, [token, isAdmin]);
+
+  if (!isAdmin) {
+    return (
+      <View>
+        <Text>You are not authorized to view this page.</Text>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
-      <Text>User List:</Text>
-      {users.map((user) => (
-        <View key={user.email}>
-          <Text>Name: {user.name}</Text>
-          <Text>Email: {user.email}</Text>
-          <Text>Contact Info: {user.contactInfo}</Text>
-          <Text>Address: {user.address}</Text>
-          <Text>Profile Picture: {user.profilePicture}</Text>
-        </View>
-      ))}
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.email}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Name: {item.name}</Text>
+            <Text>Email: {item.email}</Text>
+            <Text>Contact Info: {item.contactInfo}</Text>
+            <Text>Address: {item.address}</Text>
+            <Text>Profile Picture: {item.profilePicture}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
