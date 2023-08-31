0c5fba5b911b269bdaa075f69e0210241d33c215
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:03:14 +0530

    First Commit

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,45 +1,42 @@
 
-import React, { createContext, useState, useEffect } from 'react';
-import { UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
+import React, { createContext, useState } from 'react';
+import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
 import { updateUserProfile } from '../apis/ProfileUpdateApi';
 
 interface UserContextProps {
-  userProfile: UserProfileResponse | null;
+  userProfile: UserProfileUpdateRequest | null;
   updateUserProfile: (updatedProfile: UserProfileUpdateRequest) => Promise<UserProfileUpdateResponse>;
 }
 
 export const UserContext = createContext<UserContextProps>({
   userProfile: null,
-  updateUserProfile: () => Promise.resolve({ success: false, message: '' }),
+  updateUserProfile: async () => {
+    throw new Error('updateUserProfile function not implemented');
+  },
 });
 
 export const UserProvider: React.FC = ({ children }) => {
-  const [userProfile, setUserProfile] = useState<UserProfileResponse | null>(null);
-
-  useEffect(() => {
-    // Fetch user profile from the API
-    const fetchUserProfile = async () => {
-      try {
-        // Make API call to get user profile
-        // const response = await getUserProfile();
-        // setUserProfile(response.user);
-        console.log('Fetching user profile...');
-      } catch (error) {
-        console.error('Error fetching user profile:', error);
-      }
-    };
-
-    fetchUserProfile();
-  }, []);
+  const [userProfile, setUserProfile] = useState<UserProfileUpdateRequest | null>(null);
 
   const handleUpdateUserProfile = async (updatedProfile: UserProfileUpdateRequest) => {
     try {
-      // Make API call to update user profile
-      // const response = await updateUserProfile(updatedProfile);
-      // setUserProfile(response.user);
-      console.log('Updating user profile...');
+      // Log: Sending update profile request
+      console.log('Sending update profile request:', updatedProfile);
+
+      const response = await updateUserProfile(updatedProfile);
+
+      // Log: Update profile response
+      console.log('Update profile response:', response);
+
+      // Update the user profile state
+      setUserProfile(updatedProfile);
+
+      return response;
     } catch (error) {
-      console.error('Error updating user profile:', error);
+      // Log: Error updating profile
+      console.error('Error updating profile:', error);
+
+      throw error;
     }
   };
 
