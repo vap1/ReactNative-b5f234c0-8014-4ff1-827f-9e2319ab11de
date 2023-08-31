2a470faa7bde95421a4314ce3ce5b800a123ddd5
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:36:08 +0530

    First Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,69 +1,53 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-
-import { UserLoginRequest } from '../types/Types';
+import { View, TextInput, Button } from 'react-native';
+import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 import { loginUser } from '../apis/AuthApi';
 
-const LoginForm = () => {
+const LoginForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
-    console.log('Login button pressed');
-    console.log('Email:', email);
-    console.log('Password:', password);
-
-    const request: UserLoginRequest = {
-      email,
-      password,
-    };
-
     try {
-      const response = await loginUser(request);
-      console.log('Login API response:', response);
-      // Handle the response and perform necessary actions
+      // Log the login request
+      console.log('Login request:', { email, password });
+
+      // Make the API call to login
+      const request: UserLoginRequest = { email, password };
+      const response: UserLoginResponse = await loginUser(request);
+
+      // Log the login response
+      console.log('Login response:', response);
+
+      // Update the user token if login is successful
+      if (response.success) {
+        console.log('User login successful');
+      } else {
+        console.log('User login failed');
+      }
     } catch (error) {
-      console.error('Login API error:', error);
-      // Handle the error and display an error message
+      // Log any errors that occur during login
+      console.error('Login error:', error);
     }
   };
 
   return (
-    <View style={styles.container}>
+    <View>
       <TextInput
-        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
-        style={styles.input}
         placeholder="Password"
-        secureTextEntry
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
       />
       <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 8,
-  },
-});
 
 export default LoginForm;
