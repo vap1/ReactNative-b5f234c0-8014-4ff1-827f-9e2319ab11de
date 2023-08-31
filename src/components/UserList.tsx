2b50785382f21c715f2837c241eb732fa34d1512
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:36:07 +0530

    First Commit

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,47 +1,48 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { User } from '../types/Types';
-import { getUsers } from '../apis/AdminApi';
+import { View, Text } from 'react-native';
+import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
+import { getAdminUserDetails } from '../apis/AdminApi';
 
 const UserList: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    fetchUsers();
-  }, []);
+    const fetchUserDetails = async () => {
+      try {
+        // Log the admin user details request
+        console.log('Admin user details request');
 
-  const fetchUsers = async () => {
-    try {
-      const response = await getUsers();
-      if (response.success) {
+        // Make the API call to get admin user details
+        const request: AdminUserDetailsRequest = { token: 'adminToken123' };
+        const response: AdminUserDetailsResponse = await getAdminUserDetails(request);
+
+        // Log the admin user details response
+        console.log('Admin user details response:', response);
+
+        // Update the users state with the fetched user details
         setUsers(response.users);
-      } else {
-        console.log('Failed to fetch users:', response.message);
+      } catch (error) {
+        // Log any errors that occur during fetching admin user details
+        console.error('Admin user details error:', error);
       }
-    } catch (error) {
-      console.log('Error fetching users:', error);
-    }
-  };
+    };
 
-  const renderUser = ({ item }: { item: User }) => (
-    <View>
-      <Text>Name: {item.name}</Text>
-      <Text>Email: {item.email}</Text>
-      <Text>Contact Info: {item.contactInfo}</Text>
-      <Text>Address: {item.address}</Text>
-      <Text>Profile Picture: {item.profilePicture}</Text>
-    </View>
-  );
+    fetchUserDetails();
+  }, []);
 
   return (
     <View>
-      <Text>User List</Text>
-      <FlatList
-        data={users}
-        renderItem={renderUser}
-        keyExtractor={(item) => item.email}
-      />
+      <Text>User List:</Text>
+      {users.map((user) => (
+        <View key={user.email}>
+          <Text>Name: {user.name}</Text>
+          <Text>Email: {user.email}</Text>
+          <Text>Contact Info: {user.contactInfo}</Text>
+          <Text>Address: {user.address}</Text>
+          <Text>Profile Picture: {user.profilePicture}</Text>
+        </View>
+      ))}
     </View>
   );
 };
