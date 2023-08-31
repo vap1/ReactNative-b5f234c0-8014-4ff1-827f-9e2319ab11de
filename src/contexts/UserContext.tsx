9ad8b18fac6022cf1b9a1dd28859958e8f9ef0b6
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:07:40 +0530

    First Commit

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,47 +1,77 @@
 
-import React, { createContext, useState } from 'react';
-import { UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
-import { updateUserProfile } from '../apis/ProfileUpdateApi';
+import React, { createContext, useState, useEffect } from 'react';
+import { UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
+import { getUserProfile, updateUserProfile } from '../apis/ProfileApi';
 
+// Step 1: Create the UserContext
 interface UserContextProps {
-  userProfile: UserProfileUpdateRequest | null;
-  updateUserProfile: (updatedProfile: UserProfileUpdateRequest) => Promise<UserProfileUpdateResponse>;
+  user: UserProfileResponse | null;
+  loading: boolean;
+  error: string | null;
+  getUserProfile: () => void;
+  updateUserProfile: (request: UserProfileUpdateRequest) => void;
 }
 
 export const UserContext = createContext<UserContextProps>({
-  userProfile: null,
-  updateUserProfile: async () => {
-    throw new Error('updateUserProfile function not implemented');
-  },
+  user: null,
+  loading: false,
+  error: null,
+  getUserProfile: () => {},
+  updateUserProfile: () => {},
 });
 
+// Step 2: Create the UserProvider component
 export const UserProvider: React.FC = ({ children }) => {
-  const [userProfile, setUserProfile] = useState<UserProfileUpdateRequest | null>(null);
+  const [user, setUser] = useState<UserProfileResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleUpdateUserProfile = async (updatedProfile: UserProfileUpdateRequest) => {
-    try {
-      // Log: Sending update profile request
-      console.log('Sending update profile request:', updatedProfile);
+  // Step 3: Define the getUserProfile function
+  const getUserProfile = () => {
+    setLoading(true);
+    setError(null);
 
-      const response = await updateUserProfile(updatedProfile);
-
-      // Log: Update profile response
-      console.log('Update profile response:', response);
-
-      // Update the user profile state
-      setUserProfile(updatedProfile);
-
-      return response;
-    } catch (error) {
-      // Log: Error updating profile
-      console.error('Error updating profile:', error);
-
-      throw error;
-    }
+    getUserProfile()
+      .then((response: UserProfileResponse) => {
+        setUser(response.user);
+      })
+      .catch((error: string) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
+  // Step 4: Define the updateUserProfile function
+  const updateUserProfile = (request: UserProfileUpdateRequest) => {
+    setLoading(true);
+    setError(null);
+
+    updateUserProfile(request)
+      .then((response: UserProfileUpdateResponse) => {
+        if (response.success) {
+          getUserProfile();
+        } else {
+          setError(response.message);
+        }
+      })
+      .catch((error: string) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  // Step 5: Fetch the user profile on component mount
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
+  // Step 6: Provide the context values to the children components
   return (
-    <UserContext.Provider value={{ userProfile, updateUserProfile: handleUpdateUserProfile }}>
+    <UserContext.Provider value={{ user, loading, error, getUserProfile, updateUserProfile }}>
       {children}
     </UserContext.Provider>
   );
