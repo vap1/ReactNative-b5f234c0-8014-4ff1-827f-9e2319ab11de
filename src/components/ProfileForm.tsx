9f5830b1f21815fc11dc7aa2bace397b87f985e5
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:58:46 +0530

    First Commit

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -2,15 +2,14 @@
 import React, { useContext, useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
-import { updateUserProfile } from '../apis/ProfileUpdateApi';
-import { User, UserProfileUpdateRequest } from '../types/Types';
+import { updateUserProfile, UserProfileUpdateRequest } from '../apis/ProfileUpdateApi';
+import { User } from '../types/Types';
 
 const ProfileForm: React.FC = () => {
   const { user, setUser } = useContext(UserContext);
   const [name, setName] = useState<string>(user.name);
-  const [contactInfo, setContactInfo] = useState<string>(user.contactInfo || '');
-  const [address, setAddress] = useState<string>(user.address || '');
-  const [profilePicture, setProfilePicture] = useState<string>(user.profilePicture || '');
+  const [contactInfo, setContactInfo] = useState<string>(user.contactInfo);
+  const [address, setAddress] = useState<string>(user.address);
 
   const handleSaveChanges = async () => {
     try {
@@ -20,29 +19,26 @@ const ProfileForm: React.FC = () => {
         name,
         contactInfo,
         address,
-        profilePicture,
       };
 
       // Make the API call to update the user profile
       const response = await updateUserProfile(request);
 
       if (response.success) {
-        // Update the user context with the updated profile information
-        const updatedUser: User = {
+        // Update the user context with the updated profile
+        setUser({
           ...user,
           name,
           contactInfo,
           address,
-          profilePicture,
-        };
-        setUser(updatedUser);
+        });
 
-        console.log('Profile updated successfully!');
+        console.log('Profile updated successfully');
       } else {
         console.log('Failed to update profile:', response.message);
       }
     } catch (error) {
-      console.log('Error updating profile:', error.message);
+      console.log('Error updating profile:', error);
     }
   };
 
@@ -56,9 +52,6 @@ const ProfileForm: React.FC = () => {
 
       <Text>Address:</Text>
       <TextInput value={address} onChangeText={setAddress} />
-
-      <Text>Profile Picture:</Text>
-      <TextInput value={profilePicture} onChangeText={setProfilePicture} />
 
       <Button title="Save Changes" onPress={handleSaveChanges} />
     </View>
