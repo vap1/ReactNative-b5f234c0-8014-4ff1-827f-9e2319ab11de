cace0eda3d18bae4373b7bf94d0ccdbce66f7212
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:25:28 +0530

    First Commit

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -8,73 +8,52 @@ const RegistrationScreen = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-
   const { loginUser } = useAuthContext();
 
   const handleRegistration = async () => {
-    setIsLoading(true);
-    setErrorMessage('');
-
+    console.log('Registration initiated');
     try {
-      // Create the user registration request object
-      const registrationRequest: UserRegistrationRequest = {
+      const request: UserRegistrationRequest = {
         name,
         email,
         password,
       };
 
-      // Call the registerUser API
-      const registrationResponse: UserRegistrationResponse = await registerUser(registrationRequest);
+      const response: UserRegistrationResponse = await registerUser(request);
+      console.log('Registration response:', response);
 
-      if (registrationResponse.success) {
-        // Registration successful, log in the user
+      if (response.success) {
+        console.log('Registration successful');
+        // Automatically log in the user after successful registration
         await loginUser(email, password);
       } else {
-        // Registration failed, display the error message
-        setErrorMessage(registrationResponse.message);
+        console.log('Registration failed:', response.message);
       }
     } catch (error) {
-      // Handle any API errors
-      setErrorMessage('An error occurred during registration. Please try again.');
+      console.log('Registration error:', error);
     }
-
-    setIsLoading(false);
   };
 
   return (
     <View>
       <Text>Registration Screen</Text>
-
       <TextInput
         placeholder="Name"
         value={name}
         onChangeText={setName}
       />
-
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
-
       <TextInput
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-
-      {isLoading ? (
-        <Text>Loading...</Text>
-      ) : (
-        <Button title="Register" onPress={handleRegistration} />
-      )}
-
-      {errorMessage ? (
-        <Text>{errorMessage}</Text>
-      ) : null}
+      <Button title="Register" onPress={handleRegistration} />
     </View>
   );
 };
