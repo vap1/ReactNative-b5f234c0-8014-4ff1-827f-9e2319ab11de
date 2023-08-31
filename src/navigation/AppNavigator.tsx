828d68b4a0af2427b5eda361bfb7b450cce2c91e
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:18:01 +0530

    First Commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -12,26 +12,30 @@ import AdminUserDetailsScreen from '../screens/AdminUserDetailsScreen';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const AuthStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }} />
-    <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-  </Stack.Navigator>
-);
+const AuthStack = () => {
+  console.log('Rendering AuthStack');
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
 
-const AppStack = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Profile" component={ProfileScreen} />
-    <Tab.Screen name="Admin User Details" component={AdminUserDetailsScreen} />
-  </Tab.Navigator>
-);
+const AppStack = () => {
+  console.log('Rendering AppStack');
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Admin User Details" component={AdminUserDetailsScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const AppNavigator = () => {
+  console.log('Rendering AppNavigator');
   const isLoggedIn = false; // Check if the user is logged in
   const isAdmin = false; // Check if the user is an admin
-
-  console.log('AppNavigator - isLoggedIn:', isLoggedIn);
-  console.log('AppNavigator - isAdmin:', isAdmin);
 
   return (
     <NavigationContainer>
