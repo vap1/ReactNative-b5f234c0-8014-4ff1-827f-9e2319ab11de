ef5e027ec897eb272b18383ee74ab85f286555ed
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:03:10 +0530

    First Commit

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -7,9 +7,9 @@ import { User } from '../types/Types';
 
 const ProfileForm: React.FC = () => {
   const { user, setUser } = useContext(UserContext);
-  const [name, setName] = useState<string>(user.name);
-  const [contactInfo, setContactInfo] = useState<string>(user.contactInfo);
-  const [address, setAddress] = useState<string>(user.address);
+  const [name, setName] = useState(user.name);
+  const [contactInfo, setContactInfo] = useState(user.contactInfo);
+  const [address, setAddress] = useState(user.address);
 
   const handleSaveChanges = async () => {
     try {
@@ -38,7 +38,7 @@ const ProfileForm: React.FC = () => {
         console.log('Failed to update profile:', response.message);
       }
     } catch (error) {
-      console.log('Error updating profile:', error);
+      console.log('Error updating profile:', error.message);
     }
   };
 
