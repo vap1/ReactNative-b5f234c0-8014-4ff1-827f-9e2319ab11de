6a98d4f10101e2ce8d99c69c87a8fc4ca125544a
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:25:33 +0530

    First Commit

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,52 +1,54 @@
 
 import React, { useContext, useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, TextInput, Button, Alert } from 'react-native';
 import { UserContext, UserProfileUpdateRequest } from '../contexts/UserContext';
 import { updateUserProfile } from '../apis/ProfileUpdateApi';
 
 const ProfileForm: React.FC = () => {
   const { user, setUser } = useContext(UserContext);
-  const [name, setName] = useState<string>(user?.name || '');
-  const [contactInfo, setContactInfo] = useState<string>(user?.contactInfo || '');
-  const [address, setAddress] = useState<string>(user?.address || '');
-  const [profilePicture, setProfilePicture] = useState<string>(user?.profilePicture || '');
+  const [name, setName] = useState(user.name);
+  const [contactInfo, setContactInfo] = useState(user.contactInfo);
+  const [address, setAddress] = useState(user.address);
 
   const handleSaveChanges = async () => {
     try {
       const request: UserProfileUpdateRequest = {
-        token: user?.token || '',
+        token: user.token,
         name,
         contactInfo,
         address,
-        profilePicture,
       };
 
       const response = await updateUserProfile(request);
       if (response.success) {
-        setUser({ ...user, name, contactInfo, address, profilePicture });
-        console.log('Profile updated successfully');
+        setUser({ ...user, name, contactInfo, address });
+        Alert.alert('Success', response.message);
       } else {
-        console.log('Failed to update profile:', response.message);
+        Alert.alert('Error', response.message);
       }
     } catch (error) {
-      console.log('Error updating profile:', error);
+      console.error('Error updating user profile:', error);
+      Alert.alert('Error', 'Failed to update user profile. Please try again.');
     }
   };
 
   return (
     <View>
-      <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} />
-
-      <Text>Contact Info:</Text>
-      <TextInput value={contactInfo} onChangeText={setContactInfo} />
-
-      <Text>Address:</Text>
-      <TextInput value={address} onChangeText={setAddress} />
-
-      <Text>Profile Picture:</Text>
-      <TextInput value={profilePicture} onChangeText={setProfilePicture} />
-
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+      />
+      <TextInput
+        value={contactInfo}
+        onChangeText={setContactInfo}
+        placeholder="Contact Info"
+      />
+      <TextInput
+        value={address}
+        onChangeText={setAddress}
+        placeholder="Address"
+      />
       <Button title="Save Changes" onPress={handleSaveChanges} />
     </View>
   );
