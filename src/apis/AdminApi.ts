3bda2c1f4e96684de01e3c5264eaf4bd39bcdeb1
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:21:38 +0530

    First Commit

--- src/apis/AdminApi.ts
+++ src/apis/AdminApi.ts
@@ -2,34 +2,38 @@
 import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
 
 // Function to generate random user data
-const generateRandomUserData = (): User => {
-  const randomId = Math.floor(Math.random() * 1000);
-  return {
-    name: `User ${randomId}`,
-    email: `user${randomId}@example.com`,
-    contactInfo: `Contact Info ${randomId}`,
-    address: `Address ${randomId}`,
-    profilePicture: `https://example.com/profile/${randomId}.jpg`,
-  };
+const generateRandomUserData = (): User[] => {
+  // Generate an array of random users
+  const users: User[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    const user: User = {
+      name: `User ${i + 1}`,
+      email: `user${i + 1}@example.com`,
+      contactInfo: `Contact Info ${i + 1}`,
+      address: `Address ${i + 1}`,
+      profilePicture: `https://example.com/profiles/user${i + 1}.jpg`,
+    };
+
+    users.push(user);
+  }
+
+  return users;
 };
 
 // Function to get admin user details
 const getAdminUserDetails = (request: AdminUserDetailsRequest): AdminUserDetailsResponse => {
-  console.log('API Call: getAdminUserDetails');
-  console.log('Request:', request);
+  console.log('Fetching admin user details...');
 
-  // Generate random user data
-  const users: User[] = [];
-  for (let i = 0; i < 10; i++) {
-    users.push(generateRandomUserData());
-  }
-
-  const response: AdminUserDetailsResponse = {
-    users,
-  };
-
-  console.log('Response:', response);
-  return response;
+  // Simulate API call delay
+  const delay = Math.floor(Math.random() * 2000) + 1000;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const users = generateRandomUserData();
+      console.log('Admin user details fetched successfully:', users);
+      resolve({ users });
+    }, delay);
+  });
 };
 
 export default {
