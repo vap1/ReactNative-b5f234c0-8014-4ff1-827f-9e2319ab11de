8a15bef1723ba1701dd2e13290e4f11e78245ed4
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 03:54:32 +0530

    First Commit

--- src/apis/ProfileApi.ts
+++ src/apis/ProfileApi.ts
@@ -1,30 +1,26 @@
 
 import { UserProfileRequest, UserProfileResponse } from '../types/Types';
 
+const API_ENDPOINT = '/api/profile';
+
 export const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
   try {
-    // Log the fetch user profile request
-    console.log('Fetch user profile request:', request);
+    console.log('Sending getUserProfile API request:', request);
 
-    // Generate random user profile data
-    const userProfile: UserProfileResponse = {
-      user: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        contactInfo: '1234567890',
-        address: '123 Main St',
-        profilePicture: 'https://example.com/profile.jpg',
-      },
+    // Simulating API call and generating random data
+    const userProfile = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      contactInfo: '1234567890',
+      address: '123 Main St',
+      profilePicture: 'https://example.com/profile.jpg',
     };
 
-    // Log the fetch user profile response
-    console.log('Fetch user profile response:', userProfile);
+    console.log('Received getUserProfile API response:', userProfile);
 
-    return userProfile;
+    return { success: true, user: userProfile };
   } catch (error) {
-    // Log any errors that occur during fetch user profile
-    console.error('Fetch user profile error:', error);
-
-    throw error;
+    console.error('Error in getUserProfile API:', error);
+    return { success: false, message: 'Failed to get user profile' };
   }
 };
