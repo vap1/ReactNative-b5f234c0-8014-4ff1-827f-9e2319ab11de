bd4c66716393461f1abd89cf623adf345a075450
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:38:02 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,66 +1,30 @@
 
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { useEffect, useContext } from 'react';
+import { View, Text } from 'react-native';
 import { useUserContext } from '../contexts/UserContext';
-import { getUserProfile, updateUserProfile, UserProfileResponse, UserProfileUpdateRequest } from '../apis/ProfileApi';
-import { User } from '../types/Types';
+import { getUserProfile, UserProfileResponse } from '../apis/ProfileApi';
 
 const ProfileScreen: React.FC = () => {
   const { user, setUser } = useUserContext();
-  const [name, setName] = useState('');
-  const [contactInfo, setContactInfo] = useState('');
-  const [address, setAddress] = useState('');
-  const [profilePicture, setProfilePicture] = useState('');
 
   useEffect(() => {
-    // Fetch user profile on component mount
+    console.log('Fetching user profile...');
     getUserProfile()
       .then((response: UserProfileResponse) => {
-        console.log('User profile fetched:', response);
+        console.log('User profile fetched successfully:', response);
         setUser(response.user);
-        setName(response.user.name);
-        setContactInfo(response.user.contactInfo);
-        setAddress(response.user.address);
-        setProfilePicture(response.user.profilePicture);
       })
-      .catch((error: any) => {
-        console.error('Error fetching user profile:', error);
+      .catch((error: Error) => {
+        console.log('Error fetching user profile:', error);
       });
   }, []);
 
-  const handleSaveChanges = () => {
-    const updatedProfile: UserProfileUpdateRequest = {
-      name,
-      contactInfo,
-      address,
-      profilePicture,
-    };
-
-    updateUserProfile(updatedProfile)
-      .then((response) => {
-        console.log('User profile updated:', response);
-        setUser({ ...user, ...updatedProfile });
-      })
-      .catch((error: any) => {
-        console.error('Error updating user profile:', error);
-      });
-  };
-
   return (
     <View>
-      <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} />
-
-      <Text>Contact Info:</Text>
-      <TextInput value={contactInfo} onChangeText={setContactInfo} />
-
-      <Text>Address:</Text>
-      <TextInput value={address} onChangeText={setAddress} />
-
-      <Text>Profile Picture:</Text>
-      <TextInput value={profilePicture} onChangeText={setProfilePicture} />
-
-      <Button title="Save Changes" onPress={handleSaveChanges} />
+      <Text>Name: {user?.name}</Text>
+      <Text>Contact Info: {user?.contactInfo}</Text>
+      <Text>Address: {user?.address}</Text>
+      <Text>Profile Picture: {user?.profilePicture}</Text>
     </View>
   );
 };
