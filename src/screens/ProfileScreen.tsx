15712956c131ef8c9cb2066175b58f0db4daf55d
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:14:03 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,34 +1,68 @@
 
-import React, { useContext, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, Image, TextInput, Button } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
+import { UserProfileResponse, UserProfileUpdateRequest } from '../types/Types';
+import { updateUserProfile } from '../apis/ProfileUpdateApi';
 
 const ProfileScreen: React.FC = () => {
-  const { user, getUserProfile } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [name, setName] = useState('');
+  const [contactInfo, setContactInfo] = useState('');
+  const [address, setAddress] = useState('');
+  const [profilePicture, setProfilePicture] = useState('');
 
   useEffect(() => {
-    console.log('Fetching user profile...');
-    getUserProfile();
-  }, []);
+    if (user) {
+      setName(user.name);
+      setContactInfo(user.contactInfo || '');
+      setAddress(user.address || '');
+      setProfilePicture(user.profilePicture || '');
+    }
+  }, [user]);
 
-  if (!user) {
-    console.log('User profile not available.');
-    return (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
+  const handleSaveChanges = async () => {
+    try {
+      const updatedProfile: UserProfileUpdateRequest = {
+        name,
+        contactInfo,
+        address,
+        profilePicture,
+      };
 
-  console.log('User profile:', user);
+      const response = await updateUserProfile(updatedProfile);
+      if (response.success) {
+        setUser((prevUser: UserProfileResponse | null) => ({
+          ...prevUser,
+          name,
+          contactInfo,
+          address,
+          profilePicture,
+        }));
+        console.log('Profile updated successfully');
+      } else {
+        console.log('Failed to update profile:', response.message);
+      }
+    } catch (error) {
+      console.log('Error updating profile:', error);
+    }
+  };
 
   return (
     <View>
-      <Text>Name: {user.name}</Text>
-      <Text>Email: {user.email}</Text>
-      <Text>Contact Info: {user.contactInfo}</Text>
-      <Text>Address: {user.address}</Text>
-      <Text>Profile Picture: {user.profilePicture}</Text>
+      <Text>Name:</Text>
+      <TextInput value={name} onChangeText={setName} />
+
+      <Text>Contact Info:</Text>
+      <TextInput value={contactInfo} onChangeText={setContactInfo} />
+
+      <Text>Address:</Text>
+      <TextInput value={address} onChangeText={setAddress} />
+
+      <Text>Profile Picture:</Text>
+      <TextInput value={profilePicture} onChangeText={setProfilePicture} />
+
+      <Button title="Save Changes" onPress={handleSaveChanges} />
     </View>
   );
 };
