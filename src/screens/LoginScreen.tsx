c81102b933ca66656be7fcdfe7ef18b10b7e092c
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 04:07:35 +0530

    First Commit

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,30 +2,50 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 
+import { loginUser, UserLoginRequest, UserLoginResponse } from '../apis/AuthApi';
+import { AuthContext } from '../contexts/AuthContext';
+
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    if (!email || !password) {
-      setErrorMessage('Please enter your email and password.');
-      return;
+  const { setToken } = React.useContext(AuthContext);
+
+  const handleLogin = async () => {
+    try {
+      // Step 1: User taps on the "Login" button
+      console.log('User tapped on the "Login" button');
+
+      // Step 2: Validate input fields
+      if (!email || !password) {
+        setError('Please enter both email and password');
+        return;
+      }
+
+      // Step 3: Create the login request object
+      const loginRequest: UserLoginRequest = {
+        email,
+        password,
+      };
+
+      // Step 4: Make the API call to login the user
+      const response: UserLoginResponse = await loginUser(loginRequest);
+      console.log('API response:', response);
+
+      // Step 5: Check the API response
+      if (response.success) {
+        // Step 6: Update the token in the AuthContext
+        setToken(response.token);
+        console.log('User logged in successfully');
+      } else {
+        setError(response.message);
+        console.log('Login failed:', response.message);
+      }
+    } catch (error) {
+      setError('An error occurred during login');
+      console.log('Login error:', error);
     }
-
-    setIsLoading(true);
-    setErrorMessage('');
-
-    // Perform API call to login user
-    console.log('Logging in user...');
-    // Your API call code here
-
-    // Simulating API call with setTimeout
-    setTimeout(() => {
-      setIsLoading(false);
-      setErrorMessage('Invalid email or password. Please try again.');
-    }, 2000);
   };
 
   return (
@@ -42,8 +62,8 @@ const LoginScreen = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} disabled={isLoading} />
-      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Button title="Login" onPress={handleLogin} />
+      {error ? <Text>{error}</Text> : null}
     </View>
   );
 };
