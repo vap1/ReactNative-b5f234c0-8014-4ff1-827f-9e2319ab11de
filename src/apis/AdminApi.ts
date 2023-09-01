000caf85c7ed18d9dac227926b5e90f67c5737dd
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 17:48:21 +0530

    First Commit

--- src/apis/AdminApi.ts
+++ src/apis/AdminApi.ts
@@ -2,31 +2,36 @@
 import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
 
 // Function to generate random user data
-const generateRandomUserData = (): User[] => {
-  const users: User[] = [];
+const generateRandomUserData = (): User => {
+  const randomName = `User ${Math.floor(Math.random() * 100)}`;
+  const randomEmail = `user${Math.floor(Math.random() * 100)}@example.com`;
+  const randomContactInfo = `Contact Info ${Math.floor(Math.random() * 100)}`;
+  const randomAddress = `Address ${Math.floor(Math.random() * 100)}`;
+  const randomProfilePicture = `https://example.com/profiles/user${Math.floor(Math.random() * 100)}.jpg`;
 
-  for (let i = 1; i <= 10; i++) {
-    const user: User = {
-      name: `User ${i}`,
-      email: `user${i}@example.com`,
-      contactInfo: `Contact Info ${i}`,
-      address: `Address ${i}`,
-      profilePicture: `https://example.com/profiles/user${i}.jpg`,
-    };
-
-    users.push(user);
-  }
-
-  return users;
+  return {
+    name: randomName,
+    email: randomEmail,
+    contactInfo: randomContactInfo,
+    address: randomAddress,
+    profilePicture: randomProfilePicture,
+  };
 };
 
-export const getAdminUserDetails = (request: AdminUserDetailsRequest): AdminUserDetailsResponse => {
-  // Log: Fetching admin user details
-  console.log('Fetching admin user details');
-
+export const getAdminUserDetails = async (request: AdminUserDetailsRequest): Promise<AdminUserDetailsResponse> => {
   try {
+    // Log: Fetching admin user details
+    console.log('Fetching admin user details');
+
+    // Simulate API call delay
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     // Generate random user data
-    const users = generateRandomUserData();
+    const users: User[] = [];
+    for (let i = 0; i < 10; i++) {
+      const randomUser = generateRandomUserData();
+      users.push(randomUser);
+    }
 
     // Log: Admin user details fetched successfully
     console.log('Admin user details fetched successfully');
@@ -38,6 +43,6 @@ export const getAdminUserDetails = (request: AdminUserDetailsRequest): AdminUser
     // Log: Error fetching admin user details
     console.error('Error fetching admin user details:', error);
 
-    throw new Error('Failed to fetch admin user details');
+    throw error;
   }
 };
