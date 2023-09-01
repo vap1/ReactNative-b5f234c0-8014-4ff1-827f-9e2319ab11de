932480b241f91600a6178a2c8113ace7593709da
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 17:55:19 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -1,30 +1,20 @@
 
 import { UserRegistrationRequest, UserRegistrationResponse } from '../types/Types';
 
-// Function to simulate the user registration API call
-export const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
+const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
   try {
-    // Log the registration request
-    console.log('User Registration Request:', request);
-
-    // Simulate API call and generate a random response
+    console.log('Registering user...');
+    // Simulate API call and generate random data
     const response: UserRegistrationResponse = {
       success: true,
-      message: 'User registration successful',
+      message: 'User registered successfully!',
     };
-
-    // Log the registration response
-    console.log('User Registration Response:', response);
-
+    console.log('Registration response:', response);
     return response;
   } catch (error) {
-    // Log any errors that occur during the API call
-    console.error('Error registering user:', error);
-
-    // Return an error response
-    return {
-      success: false,
-      message: 'Error registering user',
-    };
+    console.log('Error occurred during user registration:', error);
+    throw error;
   }
 };
+
+export default registerUser;
