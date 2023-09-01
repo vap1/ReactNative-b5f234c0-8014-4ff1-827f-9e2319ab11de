0df432871cce90a51bc7d35b2d297b2b54f6d182
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 17:42:04 +0530

    First Commit

--- src/apis/AuthApi.ts
+++ src/apis/AuthApi.ts
@@ -1,19 +1,38 @@
 
 import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 
+// Function to simulate the login API call
 const loginUser = async (request: UserLoginRequest): Promise<UserLoginResponse> => {
-  console.log('Calling loginUser API');
-  console.log('Request:', request);
+  try {
+    // Log: Sending login request to the server
+    console.log('Sending login request to the server');
 
-  // Simulating API call and generating random data
-  const response: UserLoginResponse = {
-    success: true,
-    message: 'Login successful',
-    token: 'random_token',
-  };
+    // Simulating API call delay
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  console.log('Response:', response);
-  return response;
+    // Generate random data for the response
+    const response: UserLoginResponse = {
+      success: true,
+      message: 'Login successful',
+      token: 'randomToken123',
+    };
+
+    // Log: Login successful
+    console.log('Login successful');
+
+    return response;
+  } catch (error) {
+    // Log: Login failed
+    console.error('Login failed:', error);
+
+    // Generate random data for the error response
+    const response: UserLoginResponse = {
+      success: false,
+      message: 'Login failed',
+    };
+
+    return response;
+  }
 };
 
 export default loginUser;
